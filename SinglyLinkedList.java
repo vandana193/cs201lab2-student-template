83810423664a67cd63cceb864987c7e1a310beec
vandana193 <tellvandu@hotmail.com>
2026-09-13 20:55:44 +0800

    solved v1

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -101,7 +101,47 @@ public class SinglyLinkedList<E extends Comparable<E>> {
 
     // write your codes here
     public void swap(){
-        
+		if (head == null || head.next == null) return;
+
+		// 1. Collect all nodes in their ORIGINAL order
+		java.util.List<Node> originalOrder = new java.util.ArrayList<Node>();
+		Node curr = head;
+		while (curr != null) {
+			originalOrder.add(curr);
+			curr = curr.next;
+		}
+
+		// 2. Create a list of nodes sorted by VALUE (Lowest to Highest)
+		java.util.List<Node> sortedOrder = new java.util.ArrayList<Node>(originalOrder);
+		
+		//sortedOrder.sort((a, b) -> Integer.compare((int) a.getElement(), (int) b.getElement())); 
+		
+		sortedOrder.sort((a, b) -> {
+			Comparable valA = (Comparable) a.getElement();
+			Comparable valB = (Comparable) b.getElement();
+			return valA.compareTo(valB);
+		});
+		
+
+		// 3. Map each node to its swapped counterpart
+		// (1st lowest <-> 1st highest, 2nd lowest <-> 2nd highest, etc.)
+		java.util.Map<Node, Node> swapMap = new java.util.HashMap<>();
+		int n = sortedOrder.size();
+		for (int i = 0; i < n; i++) {
+			Node lowest = sortedOrder.get(i);
+			Node highest = sortedOrder.get(n - 1 - i);
+			swapMap.put(lowest, highest);
+		}
+
+		// 4. Re-link the nodes in original positional order using mapped nodes
+		head = swapMap.get(originalOrder.get(0));
+		curr = head;
+		for (int i = 1; i < originalOrder.size(); i++) {
+			curr.next = swapMap.get(originalOrder.get(i));
+			curr = curr.next;
+		}
+		tail = curr;
+		curr.next = null;         
 
     }
    
